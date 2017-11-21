f305269af3cef0232152fccd079129d893329957
ashdeepti90 <ashdeepti90@gmail.com>
2017-11-21 16:29:12 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,8 +1,16 @@
+// C program to print all permutations statisfying condition sum(arr[i]*i)<=n
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int outputCount;
+int outputCount; //variable to check the number of valid permutations
 
+// <summary>
+// Print permuted array satisfying condition sum(arr[i]*i)<=n
+// </summary>
+// <param name="arr">Permuted Array</param>
+// <param name="x">Array Size</param>
+// <param name="n">Sum for valid permutation</param>
 void print(int arr[], int x, int n) {
 	int sum = 0;
 
@@ -18,9 +26,16 @@ void print(int arr[], int x, int n) {
 	}
 }
 
+// <summary>
+// Generate all Permutations
+// </summary>
+// <param name="arr">Permuted Array</param>
+// <param name="x">Array Size</param>
+// <param name="n">Sum for valid permutation</param>
+// <param name="st">Start index for permutations</param>
 void permuteUtil(int arr[], int x, int n, int st) {
 	if (st == x - 1) {
-		print(arr, x, n);
+		print(arr, x, n); //Print all the leaf nodes of the recursion tree
 		return;
 	}
 	int tmp;
@@ -38,6 +53,11 @@ void permuteUtil(int arr[], int x, int n, int st) {
 	}
 }
 
+// <summary>
+// Generate all Permutations satisfying condition sum(arr[i]*i)<=n
+// </summary>
+// <param name="x">Array Size</param>
+// <param name="n">Sum for valid permutation</param>
 void xpermute(int x, int n) {
 	int * arr = (int *)malloc(sizeof(int)* x);
 
@@ -47,6 +67,9 @@ void xpermute(int x, int n) {
 	permuteUtil(arr, x, n, 0);
 }
 
+// <summary>
+// Driver Program
+// </summary>
 int main() {
 	int x, n;
 	scanf("%d%d", &x, &n);
